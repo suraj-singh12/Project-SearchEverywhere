19418e5c33b52ddc6fc7ecc48928d4ec20822021
vishnu <vishnuvicky3636@gmail.com>
2021-10-02 01:13:11 +0530

    dependencies for Fedora in fedoradep.sh

--- fedoradep.sh
+++ fedoradep.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo dnf check-update
-sudo dnf install build-essential -y 
+sudo dnf check-update 
 sudo dnf install python3 -y
-sudo dnf install libpoppler-cpp-dev pkg-config python-dev python3-pip -y
+sudo yum install gcc-c++ pkgconfig poppler-cpp-devel python3-devel
 sudo dnf install figlet -y
 sudo dnf install unoconv -y
 pip install pdftotext 

917c05e70f740d7a358a73140147e318dbb55e19
vishnu <vishnuvicky3636@gmail.com>
2021-10-02 00:23:05 +0530

    simplified

--- fedoradep.sh
+++ fedoradep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo dnf check-update
-sudo dnf install build-essential --assumeyes 
-sudo dnf install python3 --assumeyes
-sudo dnf install libpoppler-cpp-dev pkg-config python-dev python3-pip --assumeyes
-sudo dnf install figlet --assumeyes
-sudo dnf install unoconv --assumeyes
+sudo dnf install build-essential -y 
+sudo dnf install python3 -y
+sudo dnf install libpoppler-cpp-dev pkg-config python-dev python3-pip -y
+sudo dnf install figlet -y
+sudo dnf install unoconv -y
 pip install pdftotext 
 sudo dnf check-update
